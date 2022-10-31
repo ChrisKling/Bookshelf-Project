450d7477c6a95c1c48b394a99836291db19aca9d
unknown <iggster14@gmail.com>
2022-10-31 12:32:02 +0100

    added Bookslist and SearchBar components, removed node_modules from repo. prepped shelves for next session

---
+++ starter/src/Components/SearchBar.js
@@ -0,0 +1,19 @@
+export const CreateSearchBar = ({ setShowSearchPage, showSearchPage }) => {
+  return (
+    <div className="search-books">
+      <div className="search-books-bar">
+        <a
+          className="close-search"
+          onClick={() => setShowSearchPage(!showSearchPage)}>
+          Close
+        </a>
+        <div className="search-books-input-wrapper">
+          <input type="text" placeholder="Search by title, and title only" />
+        </div>
+      </div>
+      <div className="search-books-results">
+        <ol className="books-grid"></ol>
+      </div>
+    </div>
+  );
+};
